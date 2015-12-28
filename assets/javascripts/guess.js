ab049b9a4afad778b8d800076f49b31d740e8f5e
marion@craftsmen.io <marion@craftsmen.io>
2015-12-28 15:46:46 +0100

    Automated commit at 2015-12-28 14:46:44 UTC by middleman-deploy 1.0.0

---
+++ assets/javascripts/guess.js
@@ -0,0 +1,33 @@
+(function($) {
+  var answers = [];
+  var right_answers = ['New Born', 'Hysteria', 'Sunburn', 'Time is running out', 'Plug in Baby'];
+
+  $(document).on('click', 'a', function(event) {
+    // stop natural behavior
+    event.preventDefault();
+    var href = $(this).attr('href');
+
+    // Do your stuff here
+    if ($(this).hasClass('js-answer')) {
+      var answer = $(this).text();
+      answers.push(answer);
+    }
+
+    if ($(this).hasClass('last-devinette')) {
+      var count = 0;
+      for (var i in answers) {
+        if (right_answers[i] == answers[i]) {
+          count++;
+        }
+      }
+
+      if (count >= 3) {
+        href = '/mariane-gift-project/win';
+      } else {
+        href = '/mariane-gift-project/fail';
+      }
+    }
+
+    link(href);
+  });
+})(jQuery);
